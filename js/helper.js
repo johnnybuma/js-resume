59afeca25a1fda86f00614978bd69de497df5d0b
Jonathan Buma <johnny.buma@gmail.com>
2015-06-15 17:47:38 -0700

    Cleaned up resumeBuilder.js and completed data-entry objects

--- js/helper.js
+++ js/helper.js
@@ -18,7 +18,7 @@ var HTMLheaderRole = '<span>%data%</span><hr/>';
 var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
 var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
 var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
-var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text">%data%</span></li>';
+var HTMLlinkedin = '<li class="flex-item"><span class="orange-text">linkedin</span><span class="white-text">%data%</span></li>';
 var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="white-text">%data%</span></li>';
 var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>';
 var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
@@ -86,6 +86,10 @@ function logClicks(x,y) {
 
 $(document).click(function(loc) {
   // your code goes here!
+  var x = loc.pageX;
+  var y = loc.pageY;
+
+  logClicks(x,y);
 });
 
 
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -14,30 +14,67 @@ var bio = {
 	"bioPic": "images/fry.jpg"
 };
 
-var education = {
-	"schools": [
+var work = [
+	{
+		"employer": "Freelance",
+		"title": "Front-end Web Developer",
+		"dates": "May 2014 - Present",
+		"location": "Los Angeles, California",
+		"description": "I have been independently studying and practicing web development since 2012. In May of 2014 I was given the opportunity to leave my previous career as a commercial electrician to refine my skills and focus my career on web development. I am currently available to consider full-time employment"
+	},
+	{
+		"employer": "Accord Electric Corp",
+		"title": "Lead Electrician",
+		"dates": "August 2012 - June 2014",
+		"location": "Huntington Beach, California",
+		"description": "I was involved in the installation of large-scale industrial electrical systems. Responsible for supervising small teams on specified installations, planning daily schedule, material inventory/orders, RFI's, QA, and testing. Notable projects: Meggit AeroSpace - Simi Valley, Zodiac AeroSpace - Huntington Beach, Jacobs Engineering - Pasadena"
+	}
+
+];
+
+var projects = [
+	
+	{
+		"title": "Foothill Construction",
+		"date": "January 2015",
+		"description": "Designed local marketing page and custom estimating software for general contractor",
+		"image": "images/foothill.jpg"
+	},
+	{
+		"title": "Green Lifestyles Network",
+		"date": "March 2015",
+		"description": "Working on redevelopment of non-profit Green Media orginizations web presence",
+		"image": "images/gln.jpg"
+	}
+
+
+];
+
+var education = [
+
 		{
-			"name": "West Jordan High School",
-			"city": "West Jordan Utah",
-			"degree": "High School Diploma",
-			"year": "2006"
+			"Course": "Front-end Web Development",
+			"School": "Udacity",
+			"Format": "Online - Guided",
+			"Dates": "June 2015 - Present",
+			"Credential": "Nano Degree"
 		},
 		{
-			"name": "Udacity",
-			"location": "Online",
-			"degree": "Front-end Web Developer",
-			"year": "2015"
-		}	
-	]
-}
+			"Course": "Introduction to CS and Programming Using Python",
+			"School": "MITx",
+			"Format": "Online - Self-paced",
+			"Dates": "June 2013 - December 2013",
+			"Credential": "Certificate"
+		},
+		{
+			"Course": "Introduction to Computer Science",
+			"School": "MITx",
+			"Format": "Online",
+			"Dates": "January 2014 - July 2014",
+			"Credential": "Certificate"
+		}
 
-var work = {
-
-}
-
-var projects = {
-
-}
+];
 
 var formattedName = HTMLheaderName.replace("%data%",bio.name);
 var formattedRole = HTMLheaderRole.replace("%data%",bio.role);
@@ -46,30 +83,87 @@ $("#header").prepend(formattedName);
 
 var formattedMobile = HTMLmobile.replace("%data%",bio.contacts.mobile);
 var formattedEmail = HTMLemail.replace("%data%",bio.contacts.email);
-var formattedLocation = HTMLlocation.replace("%data%",bio.location);
+var formattedGithub = HTMLgithub.replace("%data%",bio.contacts.github);
+var formattedLinkedin = HTMLlinkedin.replace("%data%",bio.contacts.linkedin);
 var formattedBioPic = HTMLbioPic.replace("%data%",bio.bioPic);
 var formattedWelcomeMsg = HTMLWelcomeMsg.replace("%data%",bio.WelcomeMsg);
 var formattedSkills = HTMLskills.replace("%data%",bio.skills);
 
-$("#main").append(formattedMobile);
-$("#main").append(formattedEmail);
+$("#topContacts").prepend(formattedMobile);
+$("#topContacts").append(formattedEmail);
+$("#topContacts").append(formattedGithub);
+$("#topContacts").append(formattedLinkedin);
+$("#footerContacts").prepend(formattedMobile);
+$("#footerContacts").append(formattedEmail);
+$("#footerContacts").append(formattedGithub);
+$("#footerContacts").append(formattedLinkedin);
+$("#header").append(formattedBioPic);
+$("#header").append(formattedWelcomeMsg);
 
-/*if(bio.skills.length > 0) {
-	console.log("its true!");
-    $("#header").append(HTMLskillsStart);
-
-	var formattedSkill = HTMLskills.replace("%data%",bio.skills[0]);
-
-	$("#skills").append(formattedSkill);
-	formattedSkill = HTMLskills.replace("%data%",bio.skills[1]);
-	
-
-}    
-*/
 $("#header").append(HTMLskillsStart);
-console.log(bio.skills.length);
-for (var i = 0; i < bio.skills.length; i++) {
 
-	var formattedSkill = HTMLskills.replace("%data%",bio.skills[i]);
-	$("#skills").append(formattedSkill);
+
+bio.skills.display = function() {
+
+	for (var i = 0; i < bio.skills.length; i++) {
+
+		var formattedSkill = HTMLskills.replace("%data%",bio.skills[i]);
+		$("#skills").append(formattedSkill);
+	}
 }
+
+work.display = function() {
+	$("#workExperience").append(HTMLworkStart);
+	for (var i = 0; i < work.length; i++) {
+		var formattedEmployer = HTMLworkEmployer.replace("%data%",work[i].employer);
+		$(".work-entry:last").append(formattedEmployer);
+		var formattedTitle = HTMLworkTitle.replace("%data%",work[i].title);
+		$(".work-entry:last").append(formattedTitle);
+		var formattedDates = HTMLworkDates.replace("%data%",work[i].dates);
+		$(".work-entry:last").append(formattedDates);
+		var formattedLocation = HTMLworkLocation.replace("%data%",work[i].location);
+		$(".work-entry:last").append(formattedLocation);
+		var formattedDescription = HTMLworkDescription.replace("%data%",work[i].description);
+		$(".work-entry:last").append(formattedDescription);
+	}
+
+}
+
+projects.display = function () {
+	$("#projects").append(HTMLprojectStart);
+
+	for (var i = 0; i < projects.length; i++) {
+		var formattedProjectTitle = HTMLprojectTitle.replace("%data%",projects[i].title);
+		$(".project-entry:last").append(formattedProjectTitle);
+		var formattedProjectDate = HTMLprojectDates.replace("%data%",projects[i].date);
+		$(".project-entry:last").append(formattedProjectDate);
+		var formattedProjectDescription = HTMLprojectDescription.replace("%data%",projects[i].description);
+		$(".project-entry:last").append(formattedProjectDescription);
+		
+	}
+
+}
+
+education.display = function() {
+	$("#education").append(HTMLschoolStart);
+
+	for (var i = 0; i < education.length; i++){
+		var formattedSchoolName = HTMLschoolName.replace("%data%",education[i].School);
+		$(".education-entry:last").append(formattedSchoolName);
+		var formattedSchoolCourse = HTMLschoolMajor.replace("%data%",education[i].Course);
+		$(".education-entry:last").append(formattedSchoolCourse);
+		var formattedSchoolLocation = HTMLschoolLocation.replace("%data%",education[i].Format);
+		$(".education-entry:last").append(formattedSchoolLocation);
+		var formattedSchoolDates = HTMLschoolDates.replace("%data%",education[i].Dates);
+		$(".education-entry:last").append(formattedSchoolDates);
+		var formattedSchoolDegree = HTMLschoolDegree.replace("%data%",education[i].Credential);
+		$(".education-entry:last").append(formattedSchoolDegree);
+
+	}	
+}
+console.log(education.length);
+bio.skills.display();
+work.display();
+projects.display();
+education.display();
+
